920073087e264b12833aa006f03d4d72bacd0732
神经蛙 <2231655359@qq.com>
2025-05-15 15:56:01 +0800

    项目初始化—引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import { createApp } from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(Vant).use(store).use(router).mount('#app')
